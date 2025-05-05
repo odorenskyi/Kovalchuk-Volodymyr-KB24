39aef867978053422fde1b2f5730985aea7ee3fe
Kovalchuk-Volodymer <vovasurma744@gmail.com>
2025-05-05 20:17:40 +0300

    add ModulseKovalchuk.h

---
+++ lab08/prj/ModulseKovalchuk.h
@@ -0,0 +1,13 @@
+#ifndef MODULSEKOVALCHUK_H_INCLUDED
+#define MODULSEKOVALCHUK_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+void calculation_s(double x, double y, double z);
+void CalculateInterest(double amount, int months)
+void ConvertSize(int slovakSize);
+void CountBits(int N);
+
+#endif // MODULSEKOVALCHUK_H_INCLUDED
