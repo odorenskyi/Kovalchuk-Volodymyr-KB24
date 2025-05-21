670e5c83ef45ee5d2bcb92bb8777588e76246b28
Kovalchuk-Volodymer <vovasurma744@gmail.com>
2025-05-21 17:37:51 +0300

    implemented tasks 10.1, 10.2, 10.3 in the static library

--- lab08/prj/ModulseKovalchuk/main.cpp
+++ lab08/prj/ModulseKovalchuk/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <iostream>
 #include <iomanip>
+#include <vector>
+#include <cstdlib>
 #include <math.h>
 #include <fstream>
 #include <string>
@@ -8,6 +10,7 @@
 #include <ctime>
 #include <sstream>
 #include <bitset>
+
 using namespace std;
 
 void calculation_s(double x, double y, double z) {
@@ -64,5 +67,99 @@ void calculation_s(double x, double y, double z) {
             if (c == target) count++;
         }
 
-        std::cout << "Результат: " << count << std::endl;
+        std::cout << "Результат: " << count << std::endl; }
+        double s_calculation(double x, double y, double z){
+        return (x + y) * z;
+    }
+
+    void Task10_1(const string& input_filename, const string& output_filename) {
+        ofstream outfile(output_filename);
+        if (!outfile.is_open()) {
+            cerr << "Помилка відкриття: " << output_filename << endl;
+            return;
+        }
+
+        // Авторські дані
+        outfile << "Авторська інформація:\n";
+        outfile << "Ім'я: Ковальчук Володимир Олегович\n";
+        outfile << "Установа: ЦНТУ, м. Кропивницький, Україна\n";
+        outfile << "Рік: 2025\n";
+
+        // Випадкове число
+        srand(time(0));
+        int random = rand() % 91 + 10;
+        outfile << "Випадкове число (10–100): " << random << "\n";
+
+        // Очікуваний текст
+        vector<string> original = {
+            "Як парость виноградної лози, плекайте мову.",
+            "Пильно й ненастанно політь бур’ян.",
+            "Чистота від сльози вона хай буде.",
+            "Вірно і слухняно нехай вона пораду служить вам,",
+            "Хоч і живе своїм живим життям."
+        };
+
+        ifstream infile(input_filename);
+        vector<string> input;
+        string line;
+        bool read_error = false;
+
+        if (infile.is_open()) {
+            while (getline(infile, line)) input.push_back(line);
+            infile.close();
+            if (!input.empty() && input.back().empty()) input.pop_back();
+        } else {
+            cerr << "Помилка читання: " << input_filename << endl;
+            read_error = true;
+        }
+
+        outfile << "Перевірка пунктуації:\n";
+        if (read_error) {
+            outfile << "Не вдалося прочитати вхідний файл.\n";
+        } else if (input == original) {
+            outfile << "Пунктуаційні помилки відсутні.\n";
+        } else {
+            outfile << "Виявлено пунктуаційні помилки або відхилення від оригіналу.\n";
+        }
+
+        outfile << "--------------------------------------------\n";
+        outfile.close();
+    }
+
+    void Task10_2(const string& filename) {
+        ofstream file(filename, ios::app);
+        if (!file.is_open()) {
+            cerr << "Помилка відкриття: " << filename << endl;
+            return;
+        }
+
+        // Алфавіт
+        file << "\nАнглійська абетка: ";
+        for (char c = 'A'; c <= 'Z'; ++c) file << c;
+        file << "\n";
+
+        // Дата і час
+        time_t t = time(nullptr);
+        tm* now = localtime(&t);
+        file << "Дата і час: " << put_time(now, "%Y-%m-%d %H:%M:%S") << "\n";
+        file << "--------------------------------------------\n";
+        file.close();
+    }
+    void Task10_3(double x, double y, double z, unsigned int b, const string& filename) {
+        ofstream file(filename, ios::app);
+        if (!file.is_open()) {
+            cerr << "Помилка запису у файл: " << filename << endl;
+            return;
+        }
+        double S = s_calculation(x, y, z);
+        file << "Результат: S = (x + y) * z\n";
+        file << "x = " << x << ", y = " << y << ", z = " << z << ", S = " << S << "\n";
+
+        file << "Шістнадцяткова система:\n";
+        file << hex << showbase;
+        file << "x = " << (int)x << ", y = " << (int)y << ", z = " << (int)z << ", S = " << (int)S << dec << "\n";
+
+        file << "Число " << b << " у двійковому коді: " << bitset<16>(b) << "\n";
+        file << "--------------------------------------------\n";
+        file.close();
     }
