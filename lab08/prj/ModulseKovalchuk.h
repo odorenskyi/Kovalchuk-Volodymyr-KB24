4eef3d43ea712bed5727bcaf5a29582ebf14c229
Kovalchuk-Volodymer <vovasurma744@gmail.com>
2025-05-21 18:35:42 +0300

    add new file.h

--- lab08/prj/ModulseKovalchuk.h
+++ lab08/prj/ModulseKovalchuk.h
@@ -6,8 +6,11 @@
 #include <string>
 
 void calculation_s(double x, double y, double z);
-void CalculateInterest(double amount, int months)
+void CalculateInterest(double amount, int months);
 void ConvertSize(int slovakSize);
 void CountBits(int N);
+void Task10_1(const std::string& input_filename, const std::string& output_filename);
+void Task10_2 (const std::string& filename);
+void Task10_3 (double x, double y, double z,unsigned int b, const std::string& filename);
 
 #endif // MODULSEKOVALCHUK_H_INCLUDED
