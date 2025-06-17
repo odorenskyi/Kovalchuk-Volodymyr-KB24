13e26bb6fd0ae239fb1f15d4071d3576bcade35e
Kovalchuk-Volodymer <vovasurma744@gmail.com>
2025-06-17 19:46:13 +0300

    added prj

---
+++ lab12/prj/Teacher/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <locale.h>
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include "ModulseKovalchuk.h"
+
+using namespace std;
+
